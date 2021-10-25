698b5bc96ac8a0a9cef5cfb37be4b71bfafa2ee0
Jiang Yue <jiangyue12392@gmail.com>
2021-10-25 23:43:18 +0800

    Miscellaneous, primality check

---
+++ primality.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'primality' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER n as parameter.
+ */
+
+string primality(int n) {
+    if (n == 1)
+        return "Not prime";
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0)
+            return "Not prime";
+    return "Prime";
+}
+
+int main()
+{
+    string p_temp;
+    getline(cin, p_temp);
+
+    int p = stoi(ltrim(rtrim(p_temp)));
+
+    for (int p_itr = 0; p_itr < p; p_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string result = primality(n);
+
+        cout << result << "\n";
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
