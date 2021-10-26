5f7be31c477315a3343e8f68497eb278b303609f
Jiang Yue <jiangyue12392@gmail.com>
2021-10-26 22:52:52 +0800

    dp, modified Kadane's algo

---
+++ maxarraysum.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the maxSubsetSum function below.
+int maxSubsetSum(vector<int> arr) {
+    int res = 0, curr = 0;
+    vector<int> dp;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (i > 1 && dp[i - 2])
+            curr = arr[i] + dp[i - 2];
+        else
+            curr = arr[i];
+        res = max(res, curr);
+        dp.push_back(res);
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int res = maxSubsetSum(arr);
+
+    cout << res << "\n";
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
